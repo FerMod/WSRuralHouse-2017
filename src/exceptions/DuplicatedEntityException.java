35153e3550a01ca8a6d572ed7b89b7d1fc644eda
FerMod <tudelaigarcia@gmail.com>
2017-03-02 23:04:52 +0100

    Added a form to transport information of the entity

--- src/exceptions/DuplicatedEntityException.java
+++ src/exceptions/DuplicatedEntityException.java
@@ -7,6 +7,8 @@ public class DuplicatedEntityException extends Exception {
 
 	private static final long serialVersionUID = 2564825147116821092L;
 
+	private Entity entInfo;
+
 	/**
 	 * Constructs a {@code DuplicatedEntityException} with a default message.
 	 */
@@ -22,6 +24,41 @@ public class DuplicatedEntityException extends Exception {
 	 */
 	public DuplicatedEntityException(String s) {
 		super(s);
+	}
+
+	public Entity getEntity() {
+		return entInfo;
+	}
+
+	public Entity addEntityInfo(String entityName, String entityValue) {
+		return entInfo = new Entity(entityName, entityValue);
+	}
+
+	public class Entity extends DuplicatedEntityException {
+
+		private static final long serialVersionUID = -3164649154662306727L;
+
+		private String name;
+		private String value;
+
+		public Entity(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + name + ": " + value + "]";
+		}
+
 	}
 
 }
