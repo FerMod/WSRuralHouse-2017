13ba80bf9377df554b1d72353fc6738036f0aadb
FosterGun <fostergun123@gmail.com>
2017-03-10 16:12:46 +0100

    Domain revised, added new classes and modifications of these.

--- src/domain/Review.java
+++ src/domain/Review.java
@@ -1,10 +1,16 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
 public class Review {
 	
 	@XmlID
@@ -33,6 +39,64 @@ public class Review {
 		
 	}
 	
+	@Enumerated
 	private ReviewState reviewState;
+	private String description;
+	
+	
+	public Review(Integer idRev, Integer idAdmin, ReviewState reviewState, String description) {
+		this.idRev = idRev;
+		this.idAdmin = idAdmin;
+		this.reviewState = reviewState;
+		this.description = description;
+	}
+
+
+	public Integer getIdRev() {
+		return idRev;
+	}
+
+
+	public void setIdRev(Integer idRev) {
+		this.idRev = idRev;
+	}
+
+
+	public Integer getIdAdmin() {
+		return idAdmin;
+	}
+
+
+	public void setIdAdmin(Integer idAdmin) {
+		this.idAdmin = idAdmin;
+	}
+
+
+	public ReviewState getReviewState() {
+		return reviewState;
+	}
+
+
+	public void setReviewState(ReviewState reviewState) {
+		this.reviewState = reviewState;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Review [idRev=" + idRev + ", idAdmin=" + idAdmin
+				+ ", reviewState=" + reviewState + ", description="
+				+ description + "]";
+	}
 
 }
