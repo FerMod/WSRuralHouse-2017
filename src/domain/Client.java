55d3371a8b11f6afe6585984b451f2ebcc9d52d1
FerMod <tudelaigarcia@gmail.com>
2017-03-16 20:15:06 +0100

    Modified the class structure
    Applied a new structure for the User domain entities.

--- src/domain/Client.java
+++ src/domain/Client.java
@@ -1,26 +1,12 @@
 package domain;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlID;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import gui.ConsoleWindow;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
-//@DiscriminatorValue("CIENT")
 public class Client extends AbstractUser {
-
-	public static void main(String[] args) {
-		new ConsoleWindow();
-		Client c = new Client("email", "user", "pass");
-		System.out.println(c.toString());
-	}
 
 	private static final long serialVersionUID = -1989696498234692075L;
 
