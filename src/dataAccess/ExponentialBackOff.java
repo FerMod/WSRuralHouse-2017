46924fe42ff488687e8d88d52d82c32a0a2c03b2
FerMod <tudelaigarcia@gmail.com>
2017-03-17 21:13:44 +0100

    Added new constructor
    Now can be added a custom error message, to show when passed the time
    limit.

--- src/dataAccess/ExponentialBackOff.java
+++ src/dataAccess/ExponentialBackOff.java
@@ -2,47 +2,55 @@ package dataAccess;
 import static java.util.Arrays.asList;
 
 import java.net.SocketTimeoutException;
-//import java.rmi.RemoteException;
+import java.rmi.RemoteException;
 import java.util.List;
 
 import javax.net.ssl.SSLHandshakeException;
-import javax.persistence.PersistenceException;
-
 import com.objectdb.o.UserException;
 
 public final class ExponentialBackOff {
 
-	private static final int[] FIBONACCI = new int[] {1, 1, 2, 3, 5, 8, 13};
+	private static final int[] FIBONACCI = new int[] {1, 1, 2, 3, 5, 8, 13, 21};
 
-	private static final List<Class<? extends Exception>> EXPECTED_COMMUNICATION_ERRORS = asList(SSLHandshakeException.class, SocketTimeoutException.class, PersistenceException.class, IllegalStateException.class, UserException.class);
+	private static final List<Class<? extends Exception>> EXPECTED_COMMUNICATION_ERRORS = asList(
+			SSLHandshakeException.class,
+			RemoteException.class,
+			SocketTimeoutException.class, 
+			UserException.class
+			);
 
 	private ExponentialBackOff() {
 	}
 
-	public static <T> T execute(ExponentialBackOffInterface<T> fn) {
+	public static <T> T execute(ExponentialBackOffInterface<T> method) {
+		return execute(method, "Failed to communicate.");
+	}
+
+	public static <T> T execute(ExponentialBackOffInterface<T> method, String exceptionMessage) {
 		for (int attempt = 0; attempt < FIBONACCI.length; attempt++) {
 			try {
-				return fn.execute();
+				return method.execute();
 			} catch (Exception e) {
-				System.err.println(e.getMessage() + "\nRetrying operation.");
+				System.err.println("\n" + e.getMessage() + "\nRetrying operation in " + FIBONACCI[attempt] + "s.");
 				handleFailure(attempt, e);
 			}
 		}
-		throw new RuntimeException( "Failed to communicate." );
+		throw new RuntimeException(exceptionMessage);
 	}
 
 	private static void handleFailure(int attempt, Exception e) {
-		if (e.getCause() != null && !EXPECTED_COMMUNICATION_ERRORS.contains( e.getCause().getClass() )) {
-			throw new RuntimeException( e );
+		if (e.getCause() != null && !EXPECTED_COMMUNICATION_ERRORS.contains(e.getCause().getClass())) {
+			throw new RuntimeException(e);
 		}
-		doWait( attempt );
+		wait(attempt);
 	}
 
-	private static void doWait(int attempt) {
+	private static void wait(int attempt) {
 		try {
-			Thread.sleep( FIBONACCI[attempt] * 1000 );
+			Thread.sleep(FIBONACCI[attempt] * 1000);
 		} catch (InterruptedException e) {
-			throw new RuntimeException( e );
+			throw new RuntimeException(e);
 		}
 	}
+
 }
