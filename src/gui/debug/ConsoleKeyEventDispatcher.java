b336dabb938c16c2c43d7cd9a6b33177e62d46ee
FerMod <tudelaigarcia@gmail.com>
2017-03-28 21:04:35 +0200

    Non important changes

--- src/gui/debug/ConsoleKeyEventDispatcher.java
+++ src/gui/debug/ConsoleKeyEventDispatcher.java
@@ -1,10 +1,15 @@
 package gui.debug;
 
 import java.awt.AWTEvent;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JRootPane;
 
 //public abstract class ConsoleKeyEvent<T> implements KeyEventDispatcher, ConsoleKeyEventDispatcher<T> {
 public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
@@ -17,6 +22,7 @@ public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
 	//	}
 
 	private DebugBorderEventListener debugEventListener;
+	private MousePointLabel mousePointLabel;
 
 	public ConsoleKeyEventDispatcher() {
 		debugEventListener = null;
@@ -37,31 +43,55 @@ public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
 			switch (e.getKeyCode()) {
 			case KeyEvent.VK_F12:
 				if(ConsoleWindow.isVisible()) {
-					System.out.println("[" + this.getClass().getName() + "]: ConsoleWindow hidden");
+					System.out.println("[" + this.getClass().getSimpleName() + "]: ConsoleWindow hidden");
 					ConsoleWindow.setVisible(false);
 				} else {
 					ConsoleWindow.setVisible(true);
-					System.out.println("[" + this.getClass().getName() + "]: ConsoleWindow visible");
+					System.out.println("[" + this.getClass().getSimpleName() + "]: ConsoleWindow visible");
 				}
 				return true;
 			case KeyEvent.VK_F11:
-				System.out.println("[" + this.getClass().getName() + "]: ConsoleWindow disposed");
+				System.out.println("[" + this.getClass().getSimpleName() + "]: ConsoleWindow disposed");
 				ConsoleWindow.dispose();
 				return true;
-			case KeyEvent.VK_F9:
-				if(debugEventListener == null) {
-					debugEventListener = new DebugBorderEventListener();					
-					Toolkit.getDefaultToolkit().addAWTEventListener(debugEventListener, AWTEvent.MOUSE_EVENT_MASK);
-					System.out.println("[" + this.getClass().getName() + "]: Border drawing enabled");
+			case KeyEvent.VK_F10:
+
+				Object o = e.getSource();
+				if(o instanceof JFrame) {
+					if(mousePointLabel == null) {
+						JFrame source = (JFrame)o;
+						System.out.println(source.toString());
+						mousePointLabel = new MousePointLabel(source);	
+					}
+					//					mousePointLabel.setVisible(true);
+					//					Toolkit.getDefaultToolkit().addAWTEventListener(mousePointLabel, AWTEvent.MOUSE_MOTION_EVENT_MASK);
+
+
+				if(!mousePointLabel.isVisible()) {
+					mousePointLabel.setVisible(true);
+					Toolkit.getDefaultToolkit().addAWTEventListener(mousePointLabel, AWTEvent.MOUSE_MOTION_EVENT_MASK);			
+					System.out.println("[" + this.getClass().getSimpleName() + "]: Mouse coordinates drawing enabled");						
 				} else {
-					debugEventListener = null;
-					Toolkit.getDefaultToolkit().removeAWTEventListener(debugEventListener);
-					System.out.println("[" + this.getClass().getName() + "]: Border drawing disabled");
+					mousePointLabel.setVisible(false);
+					Toolkit.getDefaultToolkit().removeAWTEventListener(mousePointLabel);				
+					System.out.println("[" + this.getClass().getSimpleName() + "]: Mouse coordinates drawing disabled");						
 				}
-				return true;
 			}
+			return true;
+		case KeyEvent.VK_F9:
+			if(debugEventListener == null) {
+				debugEventListener = new DebugBorderEventListener();					
+				Toolkit.getDefaultToolkit().addAWTEventListener(debugEventListener, AWTEvent.MOUSE_MOTION_EVENT_MASK);
+				System.out.println("[" + this.getClass().getSimpleName() + "]: Border drawing enabled");
+			} else {
+				Toolkit.getDefaultToolkit().removeAWTEventListener(debugEventListener);
+				debugEventListener = null;
+				System.out.println("[" + this.getClass().getSimpleName() + "]: Border drawing disabled");
+			}
+			return false;
 		}
-		//The KeyEvent is passed to the next KeyEventDispatcher in the chain
-		return false;
 	}
+	//The KeyEvent is passed to the next KeyEventDispatcher in the chain
+	return false;
+}
 }
--- src/gui/debug/DebugBorderEventListener.java
+++ src/gui/debug/DebugBorderEventListener.java
@@ -2,43 +2,76 @@ package gui.debug;
 import java.awt.AWTEvent;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Event;
 import java.awt.Graphics;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.AWTEventListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.beans.EventHandler;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.RootPaneContainer;
+import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.MouseInputListener;
 
-public class DebugBorderEventListener implements AWTEventListener {
+public class DebugBorderEventListener implements AWTEventListener{
 	
+	private static JComponent source;
+
 	public void eventDispatched(AWTEvent event) {
-		
+
 		Object o = event.getSource();
-		
+		System.out.println(o.toString());
 		if (o instanceof JComponent) {
-			JComponent source = (JComponent) o;
-			Border border = source.getBorder();
+
+			source = (JComponent) o;
+			source.getRootPane().add(new Canvas());
+			System.out.println(MouseInfo.getPointerInfo().getLocation());			
+
+
+			Border border = source.getBorder();		
 			
-			switch (event.getID()) {
-			
-			case MouseEvent.MOUSE_ENTERED:
-				if (border != null) {
-					
-					source.setBorder(new DebugBorder(border));
-				}
-				break;
-			case MouseEvent.MOUSE_EXITED:
-				if (border != null && border instanceof DebugBorder) {
-					source.setBorder(((DebugBorder) border).getDelegate());
-				}
-				break;
-			}
-			
+			//Compound borders
+			Border compound;
+			Border redline = BorderFactory.createLineBorder(Color.red);
+
+			//Add a red outline to the frame.
+			compound = BorderFactory.createCompoundBorder(redline, border);
+			source.setBorder(compound);
+
+			//Add a title to the red-outlined frame.
+			compound = BorderFactory.createTitledBorder(
+					compound,
+					source.getClass().getName(),
+					TitledBorder.CENTER,
+					TitledBorder.BELOW_BOTTOM);
+			source.setBorder(compound);
+
 		}
-		
+
 	}
-	
+
+	class Canvas extends JComponent {
+
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+			g.drawRect (getWidth(), getHeight(), 30, 30);  
+		}
+
+	}
+
 	private static class RectDraw extends JComponent {
 
 		protected void paintComponent(Graphics g) {
@@ -46,7 +79,7 @@ public class DebugBorderEventListener implements AWTEventListener {
 			g.drawRect(230,80,10,10);  
 			g.setColor(Color.RED);  
 			//g.fillRect(230,80,10,10);  
-			
+
 		}
 
 		public Dimension getPreferredSize() {
@@ -54,7 +87,21 @@ public class DebugBorderEventListener implements AWTEventListener {
 		}
 
 	}
-	
+
+	public static Component findComponentUnderGlassPaneAt(Point p, Component top) {
+
+		Component c = null;
+
+		if (top.isShowing()) {
+			if (top instanceof RootPaneContainer)
+				c = ((RootPaneContainer) top).getLayeredPane().findComponentAt(SwingUtilities.convertPoint(top, p, ((RootPaneContainer) top).getLayeredPane()));
+			else
+				c = ((Container) top).findComponentAt(p);
+		}
+
+		return c;
+	}
+
 }
 
 //package gui.debug;
