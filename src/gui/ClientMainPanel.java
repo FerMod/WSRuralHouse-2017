f5e98e423e688b37502ae8ec74d1f6ed29ff7ed6
FerMod <tudelaigarcia@gmail.com>
2017-04-04 00:11:14 +0200

    Added and removed some features
    Removed the details button
    Added the OfferInfoDialog, still needs some work

--- src/gui/ClientMainPanel.java
+++ src/gui/ClientMainPanel.java
@@ -33,6 +33,7 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -44,6 +45,7 @@ import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.RowFilter;
 import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -61,6 +63,8 @@ import javax.swing.table.TableRowSorter;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Highlighter;
+
+import domain.Offer;
 import gui.components.TextPrompt;
 
 public class ClientMainPanel extends JPanel {
@@ -70,9 +74,9 @@ public class ClientMainPanel extends JPanel {
 
 	private static final long serialVersionUID = 3063325462028186709L;
 
+	private JFrame frame;
 	//	private JFormattedTextField minPriceField
 	private JFormattedTextField maxPriceField;
-
 	private JTextField searchField;
 	private JTable table;
 	private TableModel tableModel;
@@ -89,8 +93,10 @@ public class ClientMainPanel extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public ClientMainPanel() {
-		
+	public ClientMainPanel(JFrame frame) {
+
+		this.frame = frame;
+
 		setLayout(new GridBagLayout());
 
 		setupNumberFormat();
@@ -175,16 +181,16 @@ public class ClientMainPanel extends JPanel {
 		gbc.gridy = 1;
 		add(getTableScrollPanel(), gbc);
 
-		gbc.anchor = GridBagConstraints.PAGE_START;		
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.weightx = 0.5;
-		gbc.weighty = 0;
-		gbc.gridwidth = 5;
-
-		gbc.insets = new Insets(10, 100, 10, 100);
-		gbc.gridx = 0;
-		gbc.gridy = 4;
-		add(getBtnDetails(), gbc);
+//		gbc.anchor = GridBagConstraints.PAGE_START;		
+//		gbc.fill = GridBagConstraints.HORIZONTAL;
+//		gbc.weightx = 0.5;
+//		gbc.weighty = 0;
+//		gbc.gridwidth = 5;
+//
+//		gbc.insets = new Insets(10, 100, 10, 100);
+//		gbc.gridx = 0;
+//		gbc.gridy = 4;
+//		add(getBtnDetails(), gbc);
 
 		//		setupClientWindow();
 
@@ -567,9 +573,9 @@ public class ClientMainPanel extends JPanel {
 					int row = table.getSelectedRow();
 					if (row < 0) {
 						//Selection got filtered away.
-						btnDetails.setEnabled(false);
+						//btnDetails.setEnabled(false);
 					} else {
-						btnDetails.setEnabled(true);
+						//btnDetails.setEnabled(true);
 						int modelRow = table.convertRowIndexToModel(row);
 						System.out.println(String.format("Selected Row in view: %d. Selected Row in model: %d.", row, modelRow));
 					}
@@ -595,7 +601,7 @@ public class ClientMainPanel extends JPanel {
 					//					Point p = me.getPoint();
 					//					int row = table.rowAtPoint(p);
 					if (me.getClickCount() == 2) {
-						JOptionPane.showMessageDialog(null,	"Double clicked the row.\nWhen implemented, more details window will show...", "WIP", JOptionPane.INFORMATION_MESSAGE);
+						JOptionPane.showMessageDialog(null,	"Double clicked the row.\nWhen implemented, info window will show...", "WIP", JOptionPane.INFORMATION_MESSAGE);
 					}
 				}
 			});
@@ -703,6 +709,7 @@ public class ClientMainPanel extends JPanel {
 				{new ImageIcon("/img/house00.png"), new CellDetails("Description of the house 7 with minor details\nMore details.", "Address 7", new Double(336.6))},
 				{new ImageIcon("/img/house00.png"), new CellDetails("Description of the house 8 with minor details", "ñeñeñe 8", new Double(63.1))},
 		};
+		
 
 		private String[] images = {"/img/house00.png", "/img/house01.png", "/img/house02.png", "/img/house03.png", "/img/house04.png"};
 
@@ -901,14 +908,14 @@ public class ClientMainPanel extends JPanel {
 			priceField.setEditable(false);
 			priceField.setFocusable(false);
 			priceField.setColumns(4);
-			
+
 			GridBagConstraints gbcPriceField = new GridBagConstraints();
 			gbcPriceField.fill = GridBagConstraints.HORIZONTAL;
 			gbcPriceField.insets = new Insets(0, 0, 0, 10);
 			gbcPriceField.gridx = 1;
 			gbcPriceField.gridy = 1;
 			panel.add(priceField, gbcPriceField);
-			
+
 
 			infoButton = new JButton("Info. ");
 			infoButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -919,7 +926,6 @@ public class ClientMainPanel extends JPanel {
 			gbcInfoButton.gridy = 0;	
 			gbcInfoButton.insets = new Insets(5, 5, 5, 5);
 			panel.add(infoButton, gbcInfoButton);
-
 		}
 
 		private void updateData(CellDetails rowContent, boolean isSelected, JTable table) {
@@ -932,8 +938,11 @@ public class ClientMainPanel extends JPanel {
 			infoButton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					System.out.println("Boton");
-
+					OfferInfoDialog dialog = new OfferInfoDialog(frame, null); //FIXME
+					dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+					//Set location relative to the parent frame. ALWAYS BEFORE SHOWING THE DIALOG.
+					dialog.setLocationRelativeTo(dialog.getParentComponent());
+					dialog.setVisible(true);
 				}
 			});
 
@@ -1008,6 +1017,7 @@ public class ClientMainPanel extends JPanel {
 	public class CellDetails {
 
 		private TableCellComponents tableDetailsCell;
+		private Offer offer;
 		private String description;
 		private String address;
 		private Double price;
