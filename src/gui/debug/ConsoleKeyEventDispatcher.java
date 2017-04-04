4f7166d81917c81d42a34c12538cbc4f60e8d324
FerMod <tudelaigarcia@gmail.com>
2017-04-04 23:51:30 +0200

    Minor changes

--- src/gui/debug/ConsoleKeyEventDispatcher.java
+++ src/gui/debug/ConsoleKeyEventDispatcher.java
@@ -1,10 +1,14 @@
 package gui.debug;
 
 import java.awt.AWTEvent;
+import java.awt.Desktop;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.swing.JFrame;
 
@@ -20,10 +24,41 @@ public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
 
 	private DebugBorderEventListener debugEventListener;
 	private MousePointLabel mousePointLabel;
+	private Konami konami;
 
 	public ConsoleKeyEventDispatcher() {
 		debugEventListener = null;
 		addKeyEventDispatcher(this);
+		konami = new Konami(new Runnable() {			
+			@Override
+			public void run() {
+				try {
+					openWebpage(new URI("https://youtu.be/zqqq8uqSDnk?t=1s"));
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		//Konamis.setEnabled(true); // ^.^
+	}
+
+	public static void openWebpage(URI uri) {
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				desktop.browse(uri);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void openWebpage(URL url) {
+		try {
+			openWebpage(url.toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void addKeyEventDispatcher(KeyEventDispatcher dispatcher) {
@@ -33,7 +68,7 @@ public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
 	public void removeKeyEventDispatcher(KeyEventDispatcher dispatcher) {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(dispatcher);
 	}
-	
+
 	public void showConsole() {
 		ConsoleWindow.setVisible(true);
 		System.out.println("[" + this.getClass().getSimpleName() + "]: ConsoleWindow visible");
@@ -42,6 +77,7 @@ public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
 	@Override
 	public boolean dispatchKeyEvent(KeyEvent e) {
 		if(e.getID() == KeyEvent.KEY_PRESSED) {
+			konami.checkCode(e.getKeyCode());
 			switch (e.getKeyCode()) {
 			case KeyEvent.VK_F12:
 				if(ConsoleWindow.isVisible()) {
@@ -69,31 +105,31 @@ public class ConsoleKeyEventDispatcher implements KeyEventDispatcher {
 					//					Toolkit.getDefaultToolkit().addAWTEventListener(mousePointLabel, AWTEvent.MOUSE_MOTION_EVENT_MASK);
 
 
-				if(!mousePointLabel.isVisible()) {
-					mousePointLabel.setVisible(true);
-					Toolkit.getDefaultToolkit().addAWTEventListener(mousePointLabel, AWTEvent.MOUSE_MOTION_EVENT_MASK);			
-					System.out.println("[" + this.getClass().getSimpleName() + "]: Mouse coordinates drawing enabled");						
-				} else {
-					mousePointLabel.setVisible(false);
-					Toolkit.getDefaultToolkit().removeAWTEventListener(mousePointLabel);				
-					System.out.println("[" + this.getClass().getSimpleName() + "]: Mouse coordinates drawing disabled");						
+					if(!mousePointLabel.isVisible()) {
+						mousePointLabel.setVisible(true);
+						Toolkit.getDefaultToolkit().addAWTEventListener(mousePointLabel, AWTEvent.MOUSE_MOTION_EVENT_MASK);			
+						System.out.println("[" + this.getClass().getSimpleName() + "]: Mouse coordinates drawing enabled");						
+					} else {
+						mousePointLabel.setVisible(false);
+						Toolkit.getDefaultToolkit().removeAWTEventListener(mousePointLabel);				
+						System.out.println("[" + this.getClass().getSimpleName() + "]: Mouse coordinates drawing disabled");						
+					}
 				}
+				return true;
+			case KeyEvent.VK_F9:
+				if(debugEventListener == null) {
+					debugEventListener = new DebugBorderEventListener();					
+					Toolkit.getDefaultToolkit().addAWTEventListener(debugEventListener, AWTEvent.MOUSE_MOTION_EVENT_MASK);
+					System.out.println("[" + this.getClass().getSimpleName() + "]: Border drawing enabled");
+				} else {
+					Toolkit.getDefaultToolkit().removeAWTEventListener(debugEventListener);
+					debugEventListener = null;
+					System.out.println("[" + this.getClass().getSimpleName() + "]: Border drawing disabled");
+				}
+				return false;
 			}
-			return true;
-		case KeyEvent.VK_F9:
-			if(debugEventListener == null) {
-				debugEventListener = new DebugBorderEventListener();					
-				Toolkit.getDefaultToolkit().addAWTEventListener(debugEventListener, AWTEvent.MOUSE_MOTION_EVENT_MASK);
-				System.out.println("[" + this.getClass().getSimpleName() + "]: Border drawing enabled");
-			} else {
-				Toolkit.getDefaultToolkit().removeAWTEventListener(debugEventListener);
-				debugEventListener = null;
-				System.out.println("[" + this.getClass().getSimpleName() + "]: Border drawing disabled");
-			}
-			return false;
 		}
+		//The KeyEvent is passed to the next KeyEventDispatcher in the chain
+		return false;
 	}
-	//The KeyEvent is passed to the next KeyEventDispatcher in the chain
-	return false;
-}
 }
---
+++ src/gui/debug/Konami.java
@@ -0,0 +1,31 @@
+package gui.debug;
+
+import java.awt.event.KeyEvent;
+
+public class Konami {
+
+	private static int[] CODE = {KeyEvent.VK_UP, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_B, KeyEvent.VK_A, KeyEvent.VK_ESCAPE};
+
+	private final Runnable runnable;
+	private int index;
+
+	public Konami(Runnable runnable) {
+		this.runnable = runnable;
+	}
+
+	public void checkCode(int code) {
+		
+		if (code == CODE[index]) {
+			index++;
+		} else {
+			index = 0;
+		}
+		
+		if (index == CODE.length) {
+			runnable.run();
+			index = 0;
+		}
+		
+	}
+	
+}
