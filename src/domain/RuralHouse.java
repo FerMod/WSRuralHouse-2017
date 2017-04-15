d0e63f4151af2a965f2b585efc0bee98d8649201
FerMod <tudelaigarcia@gmail.com>
2017-04-15 16:31:04 +0200

    Changes to RuralHouse
    The variables which used another domains Objects Integer values (the id)
    now are the objects itself.
    
    Added new constructors. The the old ones are left to allow backward
    compatibility. This means that only must be use the non deprecated
    constructors.

--- src/domain/RuralHouse.java
+++ src/domain/RuralHouse.java
@@ -5,29 +5,42 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.Vector;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.swing.ImageIcon;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import javax.persistence.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
 public class RuralHouse implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * When no image is available this one will be used 
+	 */
+	private static final ImageIcon NO_IMAGE_AVAILABLE =  new ImageIcon("img/no_image_available.png");
 
 	@XmlID
 	@XmlJavaTypeAdapter(IntegerAdapter.class)
 	@Id
 	@GeneratedValue
 	private Integer id;
+	private String name;
 	private String description;
-	private int city; 
-	private int ownerId;
-	private int reviewId;
-	private String[] images;
+	private City city; 
+	private String address;
+	private Owner owner;
+	private Review review;
+	/**
+	 * The image in the first position (<i>index 0</i>) of the {@code Vector} will be used as the
+	 * rural house image icon.
+	 */
+	private Vector<ImageIcon> images;
 	private String[] tags;                
 
 	public Vector<Offer> offers;
@@ -35,10 +48,50 @@ public class RuralHouse implements Serializable {
 	public RuralHouse() {
 	}
 
-	public RuralHouse(String description, int city) {
+	/**
+	 * This constructor only remains here as backward compatibility.
+	 * <p>
+	 * <strong><em>This will be removed in a nearby future!<em><strong>
+	 */
+	@Deprecated
+	public RuralHouse(String description, City city) {
+		this(null, null, description, city, null);
+	}
+	
+	/**
+	 * Constructor of {@code RuralHouse}.
+	 * 
+	 * @param owner the owner of the rural house
+	 * @param description a short text that describes the rural house 
+	 * @param city the city where the rural house is located
+	 * @param address the address where the rural house is
+	 */
+	public RuralHouse(Owner owner, String description, City city, String address) {
+		this(owner, null, description, city, address);
+	}
+	
+	/**
+	 * Constructor of {@code RuralHouse}.
+	 * <p>
+	 * The {@code ReviewState} of the rural house will be {@code AWAITING} as default until a review is made by an administrator
+	 * 
+	 * @param owner the owner of the rural house
+	 * @param name the name of that is given to the rural house
+	 * @param description a short text that describes the rural house 
+	 * @param city the city where the rural house is located
+	 * @param address the address where the rural house is
+	 * 
+	 * @see Review.ReviewState
+	 */
+	public RuralHouse(Owner owner, String name, String description, City city, String address) {
+		this.owner = owner;
+		this.name = name;
 		this.description = description;
 		this.city = city;
+		this.address = address;
+		this.review = new Review();
 		offers = new Vector<Offer>();
+		images = new Vector<ImageIcon>();
 	}
 
 	public Integer getId() {
@@ -49,29 +102,99 @@ public class RuralHouse implements Serializable {
 		this.id = houseNumber;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
-		this.description=description;
+		this.description = description;
 	}
 
-
-	public int getCity() {
+	public City getCity() {
 		return city;
 	}
 
-	public void setCity(int city) {
+	public void setCity(City city) {
 		this.city = city;
 	}
 
-	public String[] getImages() {
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * Get the <code>Vector</code> containing objects of type <code>ImageIcon</code> 
+	 * 
+	 * @return the image vector
+	 */
+	public Vector<ImageIcon> getImages() {
 		return images;
 	}
 
-	public void setImages(String[] images) {
+	/**
+	 * Set the <code>Vector</code> of <code>ImageIcon</code>
+	 * 
+	 * @param images the <code>ImageIcon</code> <code>Vector</code>
+	 */
+	public void setImages(Vector<ImageIcon>  images) {
 		this.images = images;
+	}
+	
+	/**
+	 * Returns the image at the specified position
+	 * 
+	 * @return the <code>ImageIcon</code> at the specified index
+	 */
+	public ImageIcon getImage(int index) {
+		if(images.size() > 0) {
+			return images.get(0);
+		} else {
+			//Return a image 
+			return NO_IMAGE_AVAILABLE;
+		}
+	}
+
+	/**
+	 * Appends the specified image to the end of the image <code>Vector</code>.
+	 * 
+	 * @param image element to be appended to the <code>Vector</code>
+	 * @return <code>true</code> if the image is added to the <code>Vector</code> 
+	 */
+	public boolean addImage(ImageIcon image) {
+		return images.add(image);
+	}
+
+	/**
+	 * Removes the image at the specified position.
+	 * 
+	 * @param index the index of the image to be removed
+	 * @return element that was removed
+	 */
+	public ImageIcon removeImage(int index) {
+		return images.remove(index);
+	}
+
+	/**
+	 * Removes the first occurrence of the specified image.
+	 * If the does not contain the image, it is unchanged.
+	 * 
+	 * @param image element to be removed from this Vector, if present
+	 * @return true if it contained the specified element
+	 */
+	public boolean removeImage(ImageIcon image) {
+		return images.remove(image);
 	}
 
 	public String[] getTags() {
@@ -82,22 +205,21 @@ public class RuralHouse implements Serializable {
 		this.tags = tags;
 	}
 
-	public int getOwnerId() {
-		return ownerId;
+	public Owner getOwner() {
+		return owner;
 	}
 
-	public void setOwnerId(int ownerId) {
-		this.ownerId = ownerId;
+	public void setOwner(Owner owner) {
+		this.owner = owner;
 	}
 
-	public int getReviewId() {
-		return reviewId;
+	public Review getReview() {
+		return review;
 	}
 
-	public void setReviewId(int reviewId) {
-		this.reviewId = reviewId;
+	public void setReview(Review review) {
+		this.review = review;
 	}
-
 
 	/**
 	 * This method creates an offer with a house number, first day, last day and price
@@ -158,7 +280,7 @@ public class RuralHouse implements Serializable {
 		Offer offer;
 		while (e.hasNext()){
 			offer=e.next();
-			if ( (offer.getFirstDay().compareTo(firstDay)>=0) && (offer.getLastDay().compareTo(lastDay)<=0)  )
+			if ( (offer.getStartDate().compareTo(firstDay)>=0) && (offer.getEndDate().compareTo(lastDay)<=0)  )
 				availableOffers.add(offer);
 		}
 		return availableOffers;
@@ -173,18 +295,16 @@ public class RuralHouse implements Serializable {
 	 * @param lastDay, last day in a period range
 	 * @return the first offer that overlaps with those dates, or null if there is no overlapping offer
 	 */
-
 	public Offer overlapsWith( Date firstDay,  Date lastDay) {
 
 		Iterator<Offer> e=offers.iterator();
 		Offer offer=null;
 		while (e.hasNext()){
 			offer=e.next();
-			if ( (offer.getFirstDay().compareTo(lastDay)<0) && (offer.getLastDay().compareTo(firstDay)>0))
+			if ( (offer.getStartDate().compareTo(lastDay)<0) && (offer.getEndDate().compareTo(firstDay)>0))
 				return offer;
 		}
 		return null;
-
 	}
 
 }
