dea32f2c0f10ac1367ba304426323b9a5f1b7141
FosterGun <fostergun123@gmail.com>
2017-05-10 20:32:42 +0200

    Graphic interface of admin will be functional very soon The methods will be tested before

--- src/gui/AdminMainPanel.java
+++ src/gui/AdminMainPanel.java
@@ -1,15 +1,30 @@
 package gui;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
 import javax.swing.JRadioButton;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+import java.util.Vector;
+
 import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+
+import domain.Review;
+import domain.Review.ReviewState;
+import domain.RuralHouse;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AdminMainPanel extends JPanel {
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private DefaultComboBoxModel<RuralHouse> ruralHouses = new DefaultComboBoxModel<RuralHouse>();
+	private Review.ReviewState rS;
 
 	/**
 	 * Create the panel.
@@ -19,17 +34,42 @@ public class AdminMainPanel extends JPanel {
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(10, 39, 276, 36);
+		initializeComboBox();
+		comboBox.setModel(ruralHouses);
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				RuralHouse rh = (RuralHouse) comboBox.getSelectedItem();
+			}
+		});
 		add(comboBox);
+		
+		
+		
+		
 		
 		JEditorPane editorPane = new JEditorPane();
 		editorPane.setBounds(10, 203, 418, 131);
 		add(editorPane);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Approved");
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rS = ReviewState.APPROVED;
+			}
+		});
+		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setBounds(10, 135, 73, 38);
 		add(rdbtnNewRadioButton);
 		
+		
+		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Rejected");
+		rdbtnNewRadioButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rS = ReviewState.REJECTED;
+			}
+		});
+		buttonGroup.add(rdbtnNewRadioButton_1);
 		rdbtnNewRadioButton_1.setBounds(91, 135, 73, 38);
 		add(rdbtnNewRadioButton_1);
 		
@@ -49,6 +89,18 @@ public class AdminMainPanel extends JPanel {
 		JButton btnNewButton = new JButton("Enviar");
 		btnNewButton.setBounds(273, 342, 155, 23);
 		add(btnNewButton);
+		
+		buttonGroup.add(rdbtnNewRadioButton);
+		buttonGroup.add(rdbtnNewRadioButton_1);
 
+	}
+	
+	public void initializeComboBox() {
+		Vector<RuralHouse> rhs = MainWindow.getBusinessLogic().getRuralHouses();
+		
+		for(RuralHouse rh : rhs) {
+			ruralHouses.addElement(rh);
+			rhs.remove(rh);
+		}
 	}
 }
