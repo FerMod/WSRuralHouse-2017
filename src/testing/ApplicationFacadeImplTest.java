5343a9f9929949be7f643bb47a8bd1b123f44195
FerMod <tudelaigarcia@gmail.com>
2018-10-06 18:19:36 +0200

    Fix unit tests

--- src/testing/ApplicationFacadeImplTest.java
+++ src/testing/ApplicationFacadeImplTest.java
@@ -1,46 +1,47 @@
 package testing;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import businessLogic.ApplicationFacadeImpl;
-import businessLogic.ApplicationFacadeInterface;
-import businessLogic.util.LogFile;
-import configuration.ConfigXML;
-import dataAccess.DataAccess;
+import static org.junit.Assume.assumeNoException;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import businessLogic.ApplicationFacadeImpl;
+import businessLogic.ApplicationFacadeInterface;
+import businessLogic.util.LogFile;
+import configuration.ConfigXML;
+import dataAccess.DataAccess;
+import domain.AbstractUser.Role;
 import domain.Admin;
 import domain.Booking;
 import domain.City;
 import domain.Client;
 import domain.Offer;
 import domain.Owner;
-import domain.RuralHouse;
-import domain.AbstractUser.Role;
 import domain.Review.ReviewState;
+import domain.RuralHouse;
 import exceptions.BadDatesException;
 import exceptions.DuplicatedEntityException;
 import exceptions.OverlappingOfferException;
 
-public class ApplicationFacadeImplTest {
+class ApplicationFacadeImplTest {
 
 	static RuralHouse rh;
 	static Admin admin;
@@ -48,13 +49,13 @@ public class ApplicationFacadeImplTest {
 	static Owner owner;
 	static City city;
 	static ApplicationFacadeInterface afi;
-	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+	static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 
-	Date startDate = null;
-	Date endDate = null;
-	Offer offer = null;
-	Booking booking = null;
-	double price = 550.0;
+	Date startDate;
+	Date endDate;
+	Offer offer;
+	Booking booking;
+	double price;
 
 	@BeforeAll
 	static void initAll() {
@@ -148,6 +149,32 @@ public class ApplicationFacadeImplTest {
 	}
 
 	@Test
+	@DisplayName("Create/Delete Offer - Correct Creation/Deletion")
+	public void testCreateDeleteOffer() {
+		try {
+			startDate = parseDate("2018/09/24");
+			endDate = parseDate("2018/10/12");
+
+			int currentOffers = afi.getOfferCount();
+			offer = createTestOffer(rh, startDate, endDate, price);
+			currentOffers++;
+			assertEquals(currentOffers, afi.getOfferCount());
+
+			afi.remove(Offer.class, offer.getId());
+			currentOffers--;
+			assertEquals(currentOffers, afi.getOfferCount());
+
+			offer = createTestOffer(rh, startDate, endDate, price);
+			currentOffers++;
+			assertEquals(currentOffers, afi.getOfferCount());
+
+			assertNotNull(offer);
+		} catch (Exception e) {
+			assumeNoException("Exception thrown when testing the creation\\deletion of an offer.", e);
+		}
+	}
+
+	@Test
 	@DisplayName("CreateBooking - Correct Creation")
 	public void testCreateBooking() {
 		try {
@@ -159,22 +186,24 @@ public class ApplicationFacadeImplTest {
 
 			assertNotNull(booking);
 		} catch (Exception e) {
-			fail("The booking dint create correctly", e);
+			assumeNoException("Exception thrown when trying to create booking.", e);
 		}
 	}
 
 	@Test
 	@DisplayName("CreateBooking - BadDatesException")
 	public void testCreateBooking2() {
+		try {
+			startDate = parseDate("2018/09/24");
+			endDate = parseDate("2000/10/12");
 
-		startDate = parseDate("2018/09/24");
-		endDate = parseDate("2000/10/12");
+			// This should throw an exception later on, but we are not testing that right now
+			offer = new Offer(startDate, endDate, price, rh);
 
-		// This should throw an exception, we are not testing that right now
-		Offer offer = new Offer(startDate, endDate, price, rh);
-
-		assertThrows(BadDatesException.class, () -> booking = afi.createBooking(client, offer, startDate, endDate));
-
+			assertThrows(BadDatesException.class, () -> booking = afi.createBooking(client, offer, startDate, endDate));
+		} catch (Exception e) {
+			fail("Expected exceptions.BadDatesException but got " + e.getClass().getCanonicalName(), e);
+		}		
 	}
 
 	@Test
@@ -186,29 +215,24 @@ public class ApplicationFacadeImplTest {
 			offer = createTestOffer(rh, startDate, endDate, price);
 			assertNotNull(offer);
 		} catch (Exception e) {
-			fail("Failed when creating offer.", e);
+			assumeNoException("Exception thrown when trying to create offer.", e);
 		}
 	}
 
 	@Test
 	@DisplayName("CreateOffer - OverlappingOfferException")
 	public void testCreateOffer2() {
-		Offer testOffer = null;
 		try {
 			startDate = parseDate("2018/09/24");
 			endDate = parseDate("2018/10/12");
-			testOffer = createTestOffer(rh, startDate, endDate, price);
-			assertNotNull(testOffer);
+			offer = createTestOffer(rh, startDate, endDate, price);
+			assertNotNull(offer);
 
 			startDate = parseDate("2018/09/24");
 			endDate = parseDate("2018/10/12");
 			assertThrows(OverlappingOfferException.class, () -> offer = afi.createOffer(rh, startDate, endDate, price));
 		} catch (Exception e) {
-			fail("Instead of OverlappingOfferException another different has thrown.", e);
-		} finally {
-			if(testOffer != null) {
-				afi.remove(testOffer);
-			}
+			fail("Expected exceptions.OverlappingOfferException but got " + e.getClass().getCanonicalName(), e);
 		}
 	}
 
@@ -220,7 +244,7 @@ public class ApplicationFacadeImplTest {
 			endDate = parseDate("1997/10/14");
 			assertThrows(BadDatesException.class, () -> offer = afi.createOffer(rh, startDate, endDate, price));
 		} catch (Exception e) {
-			fail("Instead of BadDatesException another different has thrown.", e);
+			fail("Expected exceptions.BadDatesException but got " + e.getClass().getCanonicalName(), e);
 		}
 	}
 
@@ -230,12 +254,16 @@ public class ApplicationFacadeImplTest {
 		try {
 			startDate = parseDate("2018/09/24");
 			endDate = parseDate("2018/10/12");
+
 			offer = createTestOffer(rh, startDate, endDate, price);
-			assertNotNull(offer);
-			Offer obtained = afi.getOffer(rh, startDate, endDate).get(0);
-			assertEquals(offer, obtained);
+
+			Offer expected = afi.find(Offer.class, offer.getId());
+			assumeTrue(expected.equals(offer), "Assumption is not true");
+
+			Offer obtained = afi.getOffers(rh, startDate, endDate).get(0);			
+			assertEquals(expected, obtained);
 		} catch (Exception e) {
-			fail("Exception thrown when trying to get offer.", e);
+			assumeNoException("Exception thrown when trying to get offer.", e);
 		}
 	}
 
@@ -245,12 +273,13 @@ public class ApplicationFacadeImplTest {
 		try {
 			startDate = parseDate("2018/09/24");
 			endDate = parseDate("1986/11/09");
+
 			assertAll("Offer BadDatesExceptions",
 				() -> assertThrows(BadDatesException.class, () -> {
 					offer = afi.createOffer(rh, startDate, endDate, price);
 				}),
 				() -> assertThrows(BadDatesException.class, () -> {
-					assertNotNull(afi.getOffer(rh, startDate, endDate).get(0));
+					assertNotNull(afi.getOffers(rh, startDate, endDate).get(0));
 				})
 			);
 		} catch (Exception e) {
@@ -258,29 +287,30 @@ public class ApplicationFacadeImplTest {
 		}
 	}
 
-	Date parseDate(String dateString) {
-		Date date = null;
-		try {
-			date = dateFormat.parse(dateString);
-		} catch (ParseException e) {
-			fail("Failed when parsing the dates.", e);
-		}
-		assertNotNull(date);
-		return date;
-	}
-
-
-	Offer createTestOffer(RuralHouse ruralHouse, Date firstDay, Date lastDay, double price) {
+	public Offer createTestOffer(RuralHouse ruralHouse, Date firstDay, Date lastDay, double price) {
 		Offer offer = null;
 		try {
 			offer = afi.createOffer(ruralHouse, firstDay, lastDay, price);
 		} catch (OverlappingOfferException e) {
-			fail("Failed bebause of overlapping offers.", e);
+			assumeNoException("Test aborted because of overlapping offers.", e);
 		} catch (BadDatesException e) {
-			fail("Failed because of bad dates.", e);
+			assumeNoException("Test aborted because of bad dates.", e);
+		} catch (Exception e) {
+			assumeNoException("Test aborted because of unexpected exception.", e);
 		}
-		assertNotNull(offer, () -> "Expected not null Offer.");
+		assumeNotNull(offer);
 		return offer;
+	}
+
+	public Date parseDate(String dateString) {
+		Date date = null;
+		try {
+			date = dateFormat.parse(dateString);
+		} catch (ParseException e) {
+			assumeNoException("Exception thrown when trying to parse date.", e);
+		}
+		assumeNotNull(date);
+		return date;
 	}
 
 }
